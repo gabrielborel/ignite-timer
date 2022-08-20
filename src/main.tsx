148555cd78e079120775a6a04f04b77e6ba597dd
Gabriel <biel_borel@hotmail.com>
2022-08-20 18:08:11 -0300

    Added BrowserRouter provider to the root

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { App } from './App';
+import { Router } from './Router';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
       <GlobalStyle />
-      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
